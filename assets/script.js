0713f754888c1df97641b983279970926a044019
Nilda <nick.ovalles@gmail.com>
2020-11-22 09:55:36 -0700

    <Pseudocode and new develop branch>

---
+++ assets/script.js
@@ -0,0 +1,26 @@
+// Create Variables Here
+
+// Question Section (Arrays, switches, brainstorm on whiteboard)
+
+// Score Section PreWork
+
+// Time/Countdown Clock to be displayed to User
+
+// Display Issues
+
+// BEGIN QUIZ
+
+// QUESTIONS SECTION 2 (correct or incorrect)
+
+// CONCLUDE QUIZ
+
+// High Scores Section (figure out how to save stuff. localStorage)
+
+// localStorage(own section???)
+
+
+
+// ALL THE EVENT LISTENERS
+
+
+
