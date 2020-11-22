71b68b6c10cc689d49b04be708d7ca77c2ae217a
Nilda <nick.ovalles@gmail.com>
2020-11-22 14:26:20 -0700

    <updates made to JS>

--- assets/script.js
+++ assets/script.js
@@ -19,39 +19,7 @@ var submitUserInfo = document.getElementById('submit-button');
 
 
 // Question Section (Arrays, switches, brainstorm on whiteboard)
-// Put arrays here! (Brainstorm questions)
-var questions = [
-    {
-        //Questions 1 (Answer is #4)
-        question: 'What is NOT a math method?: ',
-        choices: ['1. Math.PI()', '2. Math.max()', '3. Math.sqrt()', '4. Math.cover()'],
-        answer: '4. Math.cover()'
-    },
-    {
-        // Question 2 (Answer is #1)
-        question: 'Math.random() method is always 0 to 1, but never actually _______. ',
-        choices: ['1. 1', '2. 0', '3. None of these', '4. Both of these'],
-        answer: '1. 1'
-    },
-    {
-        // Question 3 (Answer is #3)
-        question: '______ is a fall back condition in case "if" does not work or is false. ',
-        choices: ['1. "Else" ', '2. "Arrays"', '3. "Else if" ', '4. All of the Above'],
-        answer: '3. "Else if" '
-    },
-    {
-        // Question 4 (Answer is #2)
-        question: 'What is NOT a data type?',
-        choices: ['1. string', '2. console.log', '3. boolean', '4. number'],
-        answer: '2. console.log'
-    },
-    {
-        // Question 5 (Answer is #4)
-        question: 'DOM stands for: ',
-        choices: ['1. Document Opportunity Mean', '2. Deploy Object Matter', '3. Decide Open Memory ' , '4. Document Object Model'],
-        answer: '4. Document Object Model'
-    }
-];
+
 
 // Score Section PreWork
 
